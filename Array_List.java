ba0fdc162cd443b90b142b19c4e0d1683cc8aef6
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-30 19:21:23 +0530

    Create Array_List.java

---
+++ Array_List.java
@@ -0,0 +1,8 @@
+package com.lists;
+import java.util.ArrayList;
+public class Array_List {
+	public static void main(String[] args) {
+		ArrayList<Integer> a=new ArrayList<>(3);
+		System.out.println(a);
+	}
+}
