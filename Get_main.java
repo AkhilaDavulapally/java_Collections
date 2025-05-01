98b3ba6de05a66e8d8adec7e4243e16d1732f333
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-01 18:16:32 +0530

    Create Get_main.java

---
+++ Get_main.java
@@ -0,0 +1,16 @@
+package com.lists;
+import java.util.ArrayList;
+public class Get_main  {
+public static void main(String[] args) {
+				ArrayList<Integer> a=new ArrayList<>();
+				a.add(5);
+				a.add(7);
+				a.add(97);
+				a.add(32);
+				System.out.println(a);
+				for(int i=0;i<a.size();i++)
+				{
+					System.out.println(a.get(i));
+				}
+			}
+}
