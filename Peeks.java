00dd2cdbf346675a7fe52a7251e262c5b40f383c
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-02 17:58:34 +0530

    Create Peeks.java

---
+++ Peeks.java
@@ -0,0 +1,13 @@
+package com.lists;
+import java.util.Stack;
+public class Peeks {
+public static void main(String[] args) {
+						Stack<Integer> s=new Stack<>();
+						s.add(10);
+						s.add(20);
+						s.add(30);
+						System.out.println(s);
+						System.out.println(s.peek());
+						System.out.println(s);
+}
+}
