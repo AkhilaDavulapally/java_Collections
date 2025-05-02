0ef1f9bb37a7b20de842c5e05084744605e6fb2a
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-02 17:56:31 +0530

    Create Linked_Hash_set.java

---
+++ Linked_Hash_set.java
@@ -0,0 +1,16 @@
+package com.lists;
+import java.util.*;
+public class Linked_Hash_set {
+							public static void main(String[] args) {
+								LinkedHashSet<Integer> lhs=new LinkedHashSet<>();
+							    lhs.add(10);
+								lhs.add(30);
+								lhs.add(20);
+								lhs.add(50);
+								System.out.println(lhs);
+								lhs.add(30);
+								lhs.add(100);
+								lhs.add(10);
+								System.out.println(lhs);
+							}
+						}
