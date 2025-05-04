cc725ad919781b446f40323623cc7964b7a3dfdd
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-04 19:03:50 +0530

    Create Sort_main.java

---
+++ Sort_main.java
@@ -0,0 +1,14 @@
+package com.lists;
+import java.util.*;
+public class Sort_main {
+		public static void main(String[] args) {
+					List<Integer> li=new ArrayList<Integer>();
+					li.add(210);
+					li.add(22);
+					li.add(-23);
+					li.add(124);
+					System.out.println(li);
+					Collections.sort(li);
+					System.out.println(li);
+				}
+}
