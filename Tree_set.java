66c8675567a74e6045b511274c01038a300f1d65
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-07 18:09:51 +0530

    Create Tree_set.java

---
+++ Tree_set.java
@@ -0,0 +1,9 @@
+package com.Sets;
+import java.util.*;
+ public class Tree_set {
+	 public static void main(String[] args) {
+		TreeSet t=new TreeSet();
+		t.add(null);
+		System.out.println(t);
+	}
+}
