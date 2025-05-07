76a78f13c5d96f56c204c1cb52fd1926a16c9759
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-05-07 18:10:28 +0530

    Create Tree_set1.java

---
+++ Tree_set1.java
@@ -0,0 +1,15 @@
+package com.Sets;
+import java.util.TreeSet;
+public class Tree_set1 {
+public static void main(String[] args) {
+			TreeSet t=new TreeSet();
+			t.add("Sai");
+			t.add("tilak");
+			t.add("Spandana");
+			t.add("akiila");
+			t.add("Akhila");
+			t.add("spandanas");
+			t.add("Spandanas");
+			System.out.println(t);
+		}
+}
